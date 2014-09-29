64309cd41f3edd78cc41c8799014c5d7facc4726
Jerry Belich <jerry@monkeytheater.com>
2014-09-29 17:42:18 -0500

    Hmm, it didn't like that.

--- src/cdam_state_controller.cpp
+++ src/cdam_state_controller.cpp
@@ -104,10 +104,10 @@ void StateController::loopState(GameState aState) {
 			LOG("* DISABLE WIFI *");
 		} else {
 			if (Spark.connected() == false) {
-				WiFi.on();
-            	WiFi.connect();
+				//WiFi.on();
+				//WiFi.connect();
 				Spark.connect();
-				Spark.syncTime();
+				//Spark.syncTime();
 			}
 		}
 		// If button 3 held, don't print, just serial output.
