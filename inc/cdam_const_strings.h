d3ff90b0002228bfbc16f5a3c8e2534ab84e7d20
Jerry Belich <jerry@monkeytheater.com>
2014-11-18 20:55:33 -0600

    Cleanup.

--- inc/cdam_const_strings.h
+++ inc/cdam_const_strings.h
@@ -5,15 +5,15 @@ namespace cdam
 {
 
 const char CDAM_TITLE[] = "The Choosatron";
-const char CDAM_SUBTITLE[] = "Deluxe Adventure Matrix v2.0.0a2\n\nby Jerry Belich - choosatron.com";
+const char CDAM_SUBTITLE[] = "Deluxe Adventure Matrix v2.0.0a3\n\nby Jerry Belich - choosatron.com";
 const char CDAM_PRESS_BUTTON[] = "Press any button to start!";
 const char CDAM_START[] = "Choose your story below!\n";
 const char CDAM_EMPTY[] = "No stories installed! For help,\nhead to choosatron.com/start\n";
 const char CDAM_STORY_BY[] = "a story by";
 const char CDAM_CREDITS[] = "Credits to %s";
-const char CDAM_POINTS[] = "%d Points";
-const char CDAM_POINTS_OUT_OF[] = "%d out of %d Points";
-const char CDAM_POINTS_PERFECT[] = "A perfect score!!!";
+//const char CDAM_POINTS[] = "%d Points";
+//const char CDAM_POINTS_OUT_OF[] = "%d out of %d Points";
+//const char CDAM_POINTS_PERFECT[] = "A perfect score!!!";
 const char CDAM_ARCADE_CREDITS[] = "CREDITS %d/%d - INSERT COIN(S)";
 const char CDAM_ARCADE_CONTINUE[] = "*INSERT %d COIN(S) TO CONTINUE*";
 const char CDAM_TEAR_GENTLY[] = "<- TEAR GENTLY TO THE SIDE ->\n- - - - - - - - - - - - - - - - ";
