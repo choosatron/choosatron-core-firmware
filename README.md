db847ff4f69635c67d16989e35078ff2bbd558e0
jerrytron <jerry@monkeytheater.com>
2015-03-24 16:03:24 -0500

    Update to match new github repository.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Choosatron README
+
+make PRODUCT_ID=7 PRODUCT_FIRMWARE_VERSION=101 APP=choosatron-core-firmware TARGET_DIR=applications/choosatron-core-firmware/build DEBUG_BUILD=y v=1
---
+++ choosatron-core-firmware.mk
@@ -0,0 +1,19 @@
+# This file is a makefile included from the top level makefile which
+# defines the sources built for the target.
+
+# Add include paths.
+INCLUDE_DIRS += applications/choosatron-core-firmware/inc
+INCLUDE_DIRS += applications/choosatron-core-firmware/lib/elapsed_time/inc
+INCLUDE_DIRS += applications/choosatron-core-firmware/lib/memory_free/inc
+INCLUDE_DIRS += applications/choosatron-core-firmware/lib/sd/inc
+INCLUDE_DIRS += applications/choosatron-core-firmware/lib/flashee/inc
+INCLUDE_DIRS += applications/choosatron-core-firmware/lib/ymodem/inc
+
+# C source files included in this build.
+CSRC +=
+
+# C++ source files included in this build.
+CPPSRC += $(call target_files,./applications/choosatron-core-firmware/,*.cpp)
+
+# ASM source files included in this build.
+ASRC +=
